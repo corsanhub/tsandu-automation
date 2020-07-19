f4f2f4db4931d5921c5f978f0b7bc8bd0f5b165e
Gerardo Corsan <gerardo.corsan@gmail.com>
2020-07-19 16:30:24 -0500

    simple -> sketch

--- sketch/Proximity/Proximity.ino
+++ sketch/Proximity/Proximity.ino
@@ -22,6 +22,8 @@ void loop() {
 
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
+    Serial.print("duration: ");
+    Serial.println(duration);
 
     // Calculating the distance
     distance = duration * 0.034 / 2;
